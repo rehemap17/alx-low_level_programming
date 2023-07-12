86ac1fba8bb24ceb2304d13e586fa54aa0ea06d2
rehemap17 <rehemap17@gmail.com>
2023-07-12 15:14:25 -0700

    0x0B. C - malloc, free Updated

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,39 +2,39 @@
 #include <stdlib.h>
 /**
  * str_concat - get ends of input and add together for size
- * @u1: input one to concat
- * @u2: input two to concat
+ * @s1: input one to concat
+ * @s2: input two to concat
  * Return: concat of s1 and s2
  */
-char *str_concat(char *u1, char *u2)
+char *str_concat(char *s1, char *s2)
 {
 	char *conct;
 	int i, ci;
 
-	if (u1 == NULL)
-		u1 = "";
-	if (u2 == NULL)
-		u2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
 	i = ci = 0;
-	while (u1[i] != '\0')
+	while (s1[i] != '\0')
 		i++;
-	while (u2[ci] != '\0')
+	while (s2[ci] != '\0')
 		ci++;
 	conct = malloc(sizeof(char) * (i + ci + 1));
 
 	if (conct == NULL)
 		return (NULL);
 	i = ci = 0;
-	while (u1[i] != '\0')
+	while (s1[i] != '\0')
 	{
-		conct[i] = u1[i];
+		conct[i] = s1[i];
 		i++;
 	}
 
-	while (u2[ci] != '\0')
+	while (s2[ci] != '\0')
 	{
-		conct[i] = u2[ci];
+		conct[i] = s2[ci];
 		i++, ci++;
 	}
 	conct[i] = '\0';
