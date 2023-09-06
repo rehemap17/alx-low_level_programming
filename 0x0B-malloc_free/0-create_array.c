f5b4c6ea1d9f7bd9a316cb223f91e9085aaa1ae9
rehemap17 <rehemap17@gmail.com>
2023-09-06 00:33:07 -0700

    0x0B-malloc_free Updated

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,12 +3,12 @@
 /**
  * create_array - create array of size size and assign char c
  * @size: size of array
- * @y: char to assign
+ * @c: char to assign
  * Description: create array of size size and assign char c
  * Return: pointer to array, NULL if fail
  *
  */
-char *create_array(unsigned int size, char y)
+char *create_array(unsigned int size, char c)
 {
 	char *str;
 	unsigned int i;
@@ -18,6 +18,6 @@ char *create_array(unsigned int size, char y)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
-		str[i] = y;
+		str[i] = c;
 	return (str);
 }
